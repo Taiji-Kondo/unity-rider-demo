7a4c522320dce3c259794d382a3359ca71e11847
taiji <k.taiji1108@gmail.com>
2022-06-27 22:09:49 +0900

    feat: Create Rigidbody Sample

--- Assets/CubeManager.cs
+++ Assets/CubeManager.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class CubeManager : MonoBehaviour
 {
+    Rigidbody rb;
     void Start()
     {
         // ↓ Can ommiting this line
         // Transform cube = GetComponent<Transform>();
+        
+        rb = GetComponent<Rigidbody>();
+        // rb.velocity = new Vector3(1, 0, 0);
     }
 
     void Update()
     {
         // transform.position += new Vector3(0.1f, 0, 0);
-        transform.Translate(new Vector3(0.1f, 0, 0));
-        transform.Rotate(new Vector3(0.1f, 0, 0));
+        // transform.Translate(new Vector3(0.1f, 0, 0));
+        // transform.Rotate(new Vector3(0.1f, 0, 0));
+        
+        rb.AddForce(new Vector3(1, 0, 0));
     }
 }
